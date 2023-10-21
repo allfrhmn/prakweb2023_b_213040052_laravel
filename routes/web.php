ab8a629c48a8b35b81a4c69b757e312efd293982
allfrhmn <allfiandirhmn@gmail.com>
2023-10-21 18:02:26 +0700

    3. Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Allfiandi Rahman",
+        "email" => "allfiandi123@gmail.com",
+        "image" => "allfiandi.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
