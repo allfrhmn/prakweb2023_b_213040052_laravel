ff03dee06be6f0f9d828083aa2abf1569bff8ed2
allfrhmn <allfiandirhmn@gmail.com>
2023-10-22 11:07:46 +0700

    7. Post Model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,10 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($slug) {
+    public function show($id) {
         return view('post', [
             "title" => "Single Post",
-            "post" => Post::find($slug)
+            "post" => Post::find($id)
         ]); 
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman Blog Spots</h1>
+    <h1 class="mb-5">Halaman Blog Spots</h1>
     @foreach ($posts as $post)
-    <article class="mb-5">
-            <h2>
-                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
-            </h2>
-            <p>{{ $post["body"] }}</p>
-        </article>
+    <article>
+            <h2><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+            <p>{{ $post->excerpt }}</p>
+    </article>
     @endforeach
 @endsection
 
