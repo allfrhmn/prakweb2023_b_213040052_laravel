01f1815a4bb0ec096ccbe1a47dc03e82b01c82a4
allfrhmn <allfiandirhmn@gmail.com>
2023-10-22 14:37:28 +0700

    8. Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2 class="mb-5">{{ $post->title }}</h2>
-        {{ $post->body }}
-    </article>
+        <h1 class="mb-5">{{ $post->title }}</h1>
+        
+        <p>By. Allfiandi Rahman in <a href="/categories/{{ $post->category->slug }}" class="text-info-emphasis">{{ $post->category->name }}</a></p>
+        
+        {!! $post->body !!}
 
-    <a class="text-info-emphasis mb-3" href="/posts">Back to Article</a>
+        <a class="text-secondary-emphasis" href="/posts">Back to Article</a>
 @endsection
