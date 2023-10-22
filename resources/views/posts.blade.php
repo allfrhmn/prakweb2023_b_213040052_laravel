b8f6779c2848cdfb38f4a5ee369ee1022b9d7668
allfrhmn <allfiandirhmn@gmail.com>
2023-10-22 09:48:48 +0700

    6. Database, Migration & Eloquent

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Halaman Blog Spots</h1>
+    <h1>Halaman Blog Spots</h1>
     @foreach ($posts as $post)
     <article class="mb-5">
             <h2>
