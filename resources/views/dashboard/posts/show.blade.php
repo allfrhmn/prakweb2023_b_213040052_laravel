a6a93f3bf241dc86d26df42493f820e5d6bc9b2a
allfrhmn <allfiandirhmn@gmail.com>
2023-10-29 14:35:21 +0700

    17. Dashboard UI

---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,21 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="container">
+        <div class="row my-3">
+            <div class="col-lg-8">
+                <h2 class="mb-3">{{ $post->title }}</h2>
+
+                <a href="/dashboard/posts" class="btn btn-success"><i class="bi bi-arrow-left"></i> Back to all my posts</a>
+                <a href="" class="btn btn-warning"><i class="bi bi-pencil"></i> Edit</a>
+                <a href="" class="btn btn-danger"><i class="bi bi-trash"></i> Delete</a>
+
+                <img src="https://picsum.photos/1200/400?d{{ $post->category->name }}" class="img-fluid mt-3" alt="{{ $post->category->name }}">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -32,7 +32,7 @@
                         <li>
                             <form action="/logout" method="post">
                                 @csrf
-                                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"> Logout</button>
+                                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-left"></i> Logout</button>
                             </form>
                         </li>
                     </ul>
